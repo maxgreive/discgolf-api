bdf55e5d45a4ebe32b09306563d310f72f205787
Max Greive <maxgreive@googlemail.com>
2024-01-29 22:25:54 +0100

    refactor

--- server.js
+++ server.js
@@ -8,37 +8,48 @@ app.use((req, res, next) => {
   next();
 });
 
+async function getTournaments() {
+  const url = 'https://turniere.discgolf.de/index.php?p=events';
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const tournaments = $('#list_tournaments tbody tr');
+  return { $, tournaments };
+}
+
 app.get('/', async (req, res) => {
   try {
-    const url = 'https://turniere.discgolf.de/index.php?p=events';
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
+    const tournamentsArray = [];
+    const { $, tournaments } = await getTournaments();
 
-    tournamentsArray = [];
-
-    const tournaments = $('#list_tournaments tbody tr');
     tournaments.each((i, el) => {
-      const tournament = $(el);
+      try {
+        const tournament = $(el);
+        const titleLink = tournament.find('td:first-child a');
+        const locationLink = tournament.find('td:nth-child(2) a');
+        const [lat, lng] = locationLink.attr('href')?.split('/place/')[1].split(',') || [null, null];
 
-      tournamentsArray.push({
-        title: tournament.find('td:first-child a').text().trim(),
-        link: tournament.find('td:first-child a').attr('href'),
-        location: tournament.find('td:nth-child(2) a').text().trim(),
-        coords: {
-          lat: parseFloat(tournament.find('td:nth-child(2) a').attr('href')?.split('/place/')[1].split(',')[0]) || null,
-          lng: parseFloat(tournament.find('td:nth-child(2) a').attr('href')?.split('/place/')[1].split(',')[1]) || null
-        },
-        dates: {
-          startTournament: new Date(parseInt(tournament.find('td:nth-child(3)').attr('data-sort')) * 1000),
-          endTournament: new Date(parseInt(tournament.find('td:nth-child(4)').attr('data-sort')) * 1000),
-          startRegistration: tournament.find('td:nth-child(5)').attr('data-sort') !== "0" ? new Date(parseInt(tournament.find('td:nth-child(5)').attr('data-sort')) * 1000) : null
-        }
-      });
+        tournamentsArray.push({
+          title: titleLink.text().trim(),
+          link: titleLink.attr('href'),
+          location: locationLink.text().trim(),
+          coords: {
+            lat: parseFloat(lat),
+            lng: parseFloat(lng)
+          },
+          dates: {
+            startTournament: new Date(parseInt(tournament.find('td:nth-child(3)').attr('data-sort')) * 1000),
+            endTournament: new Date(parseInt(tournament.find('td:nth-child(4)').attr('data-sort')) * 1000),
+            startRegistration: tournament.find('td:nth-child(5)').attr('data-sort') !== "0" ? new Date(parseInt(tournament.find('td:nth-child(5)').attr('data-sort')) * 1000) : null
+          }
+        });
+      } catch (err) {
+        console.error(`Error while parsing tournament ${i}: ${err.message}`);
+      }
     });
     res.send(JSON.stringify(tournamentsArray));
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
-})
+});
 
 app.listen(port, () => console.log(`Server has started on port ${port}`))
