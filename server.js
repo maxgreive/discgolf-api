d4c63d93d7834e58edb9d782f5903dea96c8073c
Max Greive <maxgreive@googlemail.com>
2024-01-29 12:51:06 +0100

    remove cors

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ const cheerio = require('cheerio');
 const app = require('express')();
 const port = process.env.PORT || 8080;
 
-app.use(require('cors')());
-
 app.get('/', async (req, res) => {
   try {
     const url = 'https://turniere.discgolf.de/index.php?p=events';
-
     const { data } = await axios.get(url);
-
     const $ = cheerio.load(data);
 
     tournamentsArray = [];
@@ -22,18 +18,19 @@ app.get('/', async (req, res) => {
       tournamentsArray.push({
         title: tournament.find('td:first-child a').text().trim(),
         link: tournament.find('td:first-child a').attr('href'),
+        location: tournament.find('td:nth-child(2) a').text().trim(),
         coords: {
           lat: parseFloat(tournament.find('td:nth-child(2) a').attr('href')?.split('/place/')[1].split(',')[0]) || null,
           lng: parseFloat(tournament.find('td:nth-child(2) a').attr('href')?.split('/place/')[1].split(',')[1]) || null
         },
         dates: {
-          startTournament: new Date(tournament.find('td:nth-child(3)').text().trim()),
-          endTournament: new Date(tournament.find('td:nth-child(4)').text().trim()),
-          startRegistration: new Date(tournament.find('td:nth-child(5)').text().trim())
+          startTournament: new Date(parseInt(tournament.find('td:nth-child(3)').attr('data-sort')) * 1000),
+          endTournament: new Date(parseInt(tournament.find('td:nth-child(4)').attr('data-sort')) * 1000),
+          startRegistration: tournament.find('td:nth-child(5)').attr('data-sort') !== "0" ? new Date(parseInt(tournament.find('td:nth-child(5)').attr('data-sort')) * 1000) : null
         }
       });
     });
-    res.send(tournamentsArray);
+    res.send(JSON.stringify(tournamentsArray));
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
