8d789b60dea4293fe5d32668d5454f35d7657831
Max Greive <maxgreive@googlemail.com>
2026-08-14 16:43:04 +0200

    style: format tournament date conditional

--- src/scrapers/tournamentsScraper.ts
+++ src/scrapers/tournamentsScraper.ts
@@ -93,7 +93,9 @@ export async function fetchOfficial(): Promise<TournamentOutput[]> {
           startTournament: tournament.timestamp_start
             ? new Date(tournament.timestamp_start * 1000)
             : null,
-          endTournament: tournament.timestamp_end ? new Date(tournament.timestamp_end * 1000) : null,
+          endTournament: tournament.timestamp_end
+            ? new Date(tournament.timestamp_end * 1000)
+            : null,
           startRegistration: tournament.timestamp_registration_phase
             ? new Date(tournament.timestamp_registration_phase * 1000)
             : null,
